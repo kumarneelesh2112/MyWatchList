51f1525517f6c218b8807f02dc98e00ed5a0c033
Neelesh kumar <57165193+kumarneelesh2112@users.noreply.github.com>
2022-02-15 12:25:15 +0530

    Add files via upload

---
+++ engine.js
@@ -0,0 +1,18 @@
+function AddShow()
+{
+    const showForm=document.querySelector(".showForm");
+
+   if(showForm.style.visibility=="hidden") showForm.style.visibility="visible" ;
+   else showForm.style.visibility="hidden" ;
+
+}
+
+function AddNote()
+{
+    
+}
+
+
+let selectedShowName,selectedShowRating,selectedShowUrl;
+
+// export{selectedShowName,selectedShowRating,selectedShowUrl};
